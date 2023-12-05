e3c387dbcf5296dca0f882239b5c1fb78f1d301e
MarcosGomezEsc <142756989+MarcosGomezEsc@users.noreply.github.com>
2023-12-05 17:02:47 -0300

    Add files via upload

--- Fetch/Fetch_Autos.jsx
+++ Fetch/Fetch_Autos.jsx
@@ -6,7 +6,7 @@ const Fetch_Autos = () => {
   useEffect(() => {
     const obtenerDatos = async () => {
       try {
-        const response = await fetch("http://localhost:3000");
+        const response = await fetch("http://localhost:3000/");
 
         if (!response.ok) {
           throw new Error("Error al obtener los datos:", response.status);
@@ -20,8 +20,31 @@ const Fetch_Autos = () => {
     };
     obtenerDatos();
   }, []);
-  console.log("datos", datos);
-  return <></>;
+  //DESDE ACA FUNCIONA BIEN
+
+  //Y ACA RETORNO
+  console.log(datos);
+  return (
+    <>
+      <h2>Desde el fetch</h2>
+      {datos.length > 0 ? (
+        <ul>
+          {datos.map((auto, index) => (
+            <li key={index}>
+              <p>Marca: {auto.Marca}</p>
+              <p>Modelo: {auto.Modelo}</p>
+              <p>Color: {auto.Color}</p>
+              <p>Precio: U$S{auto.Precio}</p>
+              <hr />
+              {/* <Botones datos={datos} setDatos={set} /> */}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No se encontraron datos</p>
+      )}
+    </>
+  );
 };
 
 export default Fetch_Autos;
